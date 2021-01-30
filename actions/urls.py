03ada7cf96c6f36602cc9f8327def40d0677e820
Evan Clark <evan.lorenzo.clark@gmail.com>
2021-01-29 20:24:33 -0800

    added command url

--- actions/urls.py
+++ actions/urls.py
@@ -5,4 +5,5 @@ from . import commands
 urlpatterns = [
     path("commands/dhelp", commands.dhelp),
     path("commands/ticket", commands.create_ticket),
+    path("commands/status-check", commands.check_status),
 ]
